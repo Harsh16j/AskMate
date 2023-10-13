3b7242eb41d4571a1cc7eeca45c0518125a08b7e
Harsh <harsh.jethwani16@gmail.com>
2023-10-13 15:14:28 -0700

    Create migration and update database for the FAQ model class.

---
+++ Data/Migrations/20231013215917_initial setup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AskMate.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FAQs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    faqQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    faqAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FAQs");
+        }
+    }
+}

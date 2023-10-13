f8efb0ad35daec56a94d4824b6ae0a7bcbd2a258
Harsh <harsh.jethwani16@gmail.com>
2023-10-13 14:56:18 -0700

    FAQs model created for storing FAQ data

---
+++ Models/FAQs.cs
@@ -0,0 +1,14 @@
+﻿namespace AskMate.Models
+{
+    public class FAQs
+    {
+        public int Id { get; set; }
+        public string faqQuestion { get; set; }
+        public string faqAnswer { get; set; }
+
+        public FAQs()
+        {
+            
+        }
+    }
+}
